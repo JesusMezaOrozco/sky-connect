3a21299c0c597433dcc5dde6f91e411e55422842
JesusMezaOrozco <mezajesusorozco@gmail.com>
2025-03-20 14:28:24 -0500

    Agregar componente de entrada y botón de búsqueda en la página principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-color-primary-gradient">SkyConnect Explorer</h1>
+      <Input
+        className="placeholder-primary text-primary h-[2.3rem] rounded-[3.5rem] border-none bg-white focus-visible:ring-0"
+        placeholder="Buscar Aeropuertos..."
+      />
+      <Button className="background-color-primary-gradient" asChild>
+        <Link href="#">
+          <Image src="/icons/search.svg" alt="Buscar" width={20} height={20} />
+          Buscar
+        </Link>
+      </Button>
     </div>
   );
 }
