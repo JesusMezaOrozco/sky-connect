bd045235e360d5adfaf60cf38c3dfd6f99fbc34c
JesusMezaOrozco <mezajesusorozco@gmail.com>
2025-03-23 17:24:50 -0500

    Eliminar archivos de API y sus interceptores para la gestión de aeropuertos

--- app/(pages)/api/airportsApi.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { AddAccessKeyParamToRequest } from "./interceptors";
-
-export const airportsApi = axios.create({
-  baseURL: `${process.env.AVIATION_STACK_BASE_URL}/airports`,
-});
-
-airportsApi.interceptors.request.use(AddAccessKeyParamToRequest);
